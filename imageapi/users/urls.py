0632c1cbb140c60160f0bc6242f5879c568af884
gprzyby <gprzybyl26@gmail.com>
2021-06-04 22:43:40 +0200

    feat: added endpoints names for url reverse in tests

--- imageapi/users/urls.py
+++ imageapi/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import UserView, create_jwt_token_view, create_cookie_authentication_view
 
 urlpatterns = [
-    path('', UserView().as_view()),
-    path('token', create_jwt_token_view),
-    path('cookie', create_cookie_authentication_view),
+    path('', UserView().as_view(), name='user_view'),
+    path('token', create_jwt_token_view, name='user_token_creation'),
+    path('cookie', create_cookie_authentication_view, name='user_cookie_creation'),
 ]
